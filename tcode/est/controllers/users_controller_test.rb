4215d634500ed7f7a3ed8200f6cefebda2596b3c
Won Wook SONG <wsong0512@gmail.com>
2015-04-20 19:52:10 +0900

    require simplecov on test helper and put a false test

--- tcode/est/controllers/users_controller_test.rb
+++ tcode/est/controllers/users_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  test 'not actually a test' do
+    assert false
+  end
   # test "the truth" do
   #   assert true
   # end
