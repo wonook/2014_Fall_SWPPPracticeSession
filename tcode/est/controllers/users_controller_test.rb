bf59cd9865bb0ae4d11e8aba689de81934303e89
Won Wook SONG <wsong0512@gmail.com>
2015-04-20 19:59:02 +0900

    make test for signup:success and failure

--- tcode/est/controllers/users_controller_test.rb
+++ tcode/est/controllers/users_controller_test.rb
@@ -4,6 +4,17 @@ class UsersControllerTest < ActionController::TestCase
   test 'not actually a test' do
     assert_not false
   end
+  test '#signup_success' do
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse(response.body)
+    assert json['user_name'] == 'testuser'
+    assert json['login_count']==1
+  end
+  test '#signup_failure_user' do
+    post :signup, {'username' => 'test', 'password' => 'testpass'}
+    json = JSON.parse(response.body)
+    assert json['error_code'] == -1
+  end
   # test "the truth" do
   #   assert true
   # end
